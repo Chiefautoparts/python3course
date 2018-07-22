e22086031d63313a19bec1a06e422faae20072db
Chiefautoparts <chiefautoparts@outlook.com>
2018-07-22 15:42:09 -0700

    first commit

---
+++ funds/ForLoopBasic.py
@@ -0,0 +1,26 @@
+# for num in range(0,151):
+# 	print(num)
+# 	
+
+# for num in range(4,1000001):
+# 	if num % 5 == 0:
+# 		print(num)
+
+# for num in range(0,101):
+# 	if num % 10 == 0:
+# 		print("Coding Dojo")
+# 	elif num % 5 == 0:
+# 		print("Coding")
+# 	else:
+# 		print(num)
+
+# total = 0
+# for num in range(0,500001):
+# 	if num % 2 == 1:
+# 		total += num
+# 	print(total)
+
+# posNum = 2018
+# while posNum > 0:
+# 	print(posNum)
+# 	posNum -= 4
